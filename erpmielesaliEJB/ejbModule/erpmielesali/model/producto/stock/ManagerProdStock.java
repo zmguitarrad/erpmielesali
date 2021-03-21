a1a37233e6a77144138c699f1bdc3f34fabd7395
ZAMIA-G <zmguitarrad@utn.edu.ec>
2021-03-20 23:33:59 -0500

    Crear Mangager Stock

---
+++ erpmielesaliEJB/ejbModule/erpmielesali/model/producto/stock/ManagerProdStock.java
@@ -0,0 +1,20 @@
+package erpmielesali.model.producto.stock;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerProdStock
+ */
+@Stateless
+@LocalBean
+public class ManagerProdStock {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerProdStock() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
